7d6aca46332f4146ff5f279c27f53c222ecf6451
Doug Osborn <d00303905@dmail.dixie.edu>
2021-01-22 19:15:32 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+let tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Visible)
+tileDisplay.setBrightness(50)
+tileDisplay.clear()
+basic.forever(function () {
+	
+})
