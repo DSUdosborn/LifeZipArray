276c9b0e2188b4ad3b25f86256fa5db0d0972985
Doug Osborn <d00303905@dmail.dixie.edu>
2021-01-25 00:03:34 -0700

    cut spaceship demo in 1/2

--- main.ts
+++ main.ts
@@ -45,7 +45,7 @@ input.onButtonPressed(Button.AB, function () {
     switch (logostate) {
         case 1: logostate += 1; genlimit = 19; blinkDelay = 400; showBlinker(); break
         case 2: logostate += 1; genlimit = 25; blinkDelay = 250; showGlider(); break
-        case 3: logostate += 1; genlimit = 45; blinkDelay = 200; showSpaceship(); break
+        case 3: logostate += 1; genlimit = 24; blinkDelay = 200; showSpaceship(); break
         case 4: logostate += 1; genlimit = 25;  blinkDelay = 100; showLines(); break
         case 5: logostate += 1; genlimit = 133; blinkDelay = 50; showSoup(); break
         default: logostate = 1; blinkDelay = 300; break;
