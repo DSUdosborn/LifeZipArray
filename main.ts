a405837102c4d6f93d947a51c1804d082ad8cbd6
Doug Osborn <d00303905@dmail.dixie.edu>
2021-02-01 21:57:28 -0700

    oops

--- main.ts
+++ main.ts
@@ -51,7 +51,7 @@ input.onButtonPressed(Button.AB, function () {
         case 2: logostate += 1; genlimit = 19; blinkDelay = 400; showBlinker(); break
         case 3: logostate += 1; genlimit = 25; blinkDelay = 250; showGlider(); break
         case 4: logostate += 1; genlimit = 21; blinkDelay = 200; showSpaceship(); break
-        case 6: logostate += 1; genlimit = 133; blinkDelay = 50; showSoup(); break
+        case 5: logostate += 1; genlimit = 133; blinkDelay = 50; showSoup(); break
         default: logostate = 1; blinkDelay = 300; showLogo(); break;
     }
     basic.pause(1000)
